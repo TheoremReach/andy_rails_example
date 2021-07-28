e0ec2d265d0592631e3bd77ed42fb1f9e5a49e89
Adam Perkins <a@aap.bz>
2021-07-28 10:27:14 -0500

    Run rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  namespace :api, defaults: {format: 'json'} do
-    namespace :v1, defaults: {format: 'json'} do
-      resources :route_callback, only: [:create, :index] 
-    end  
-    namespace :v2, defaults: {format: 'json'} do
-      resources :route_callback, only: [:create] 
-    end  
-
-  end
-
-  get "/health_check", to: "static#health_check"
-
-  get "/" => redirect("https://google.com")
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
