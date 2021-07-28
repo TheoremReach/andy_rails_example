f8673b5c8ae3f06cc2097091cc88be5db77c2496
Adam Perkins <a@aap.bz>
2021-07-28 10:42:22 -0500

    more debug?

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module TrCallbackProxy
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before Rack::Sendfile, ActionDispatch::DebugLocks
+    
   end
 end
